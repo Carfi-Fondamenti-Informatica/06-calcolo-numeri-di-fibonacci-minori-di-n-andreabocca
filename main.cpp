28427b410e4a57ef761895b6287edb560184f85e
andreabocca <127606319+andreabocca@users.noreply.github.com>
2023-05-09 17:12:49 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int x=0,n=0,k=1,y=1;
+    cin>>n;
+    cout<<1<<endl;
+    while (k<n) {
+        if (n>=(x+y)) {
+            cout << x + y << endl;
+            if (x < y) {
+                x = x + y;
+            } else {
+                y = x + y;
+            }
+            k++;
+        }
+    }
+    return 0;
 }
